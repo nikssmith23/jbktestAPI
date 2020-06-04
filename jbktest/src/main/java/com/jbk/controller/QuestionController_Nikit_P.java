a6e4e9c0f028deed88af394fd224ab0595a25b86
nikssmith23 <nikssmith23@gmail.com>
2020-06-04 15:11:30 +0530

    Add files via upload

---
+++ jbktest/src/main/java/com/jbk/controller/QuestionController_Nikit_P.java
@@ -0,0 +1,47 @@
+package com.jbk.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("testapi")
+public class QuestionsController_Nikit_P {
+
+	public Question getQuestion(int qid) {
+
+		Question question = new Question();
+		return question;
+	}
+
+	public List<Question> getAllQuestions() {
+
+		List<Question> list = new ArrayList<>();
+		return list;
+	}
+
+	public boolean updateQuestion(int qid) {
+		return false;
+	}
+
+	public boolean deleteQuestion(int qid) {
+		return false;
+	}
+
+	public List<Question> questionPaper(int count) {
+
+		List<Question> list = new ArrayList<>();
+		if (count == 10) {
+			System.out.println("10 question selected");
+		} else if (count == 25) {
+			System.out.println("25 question selected");
+		} else if (count == 30) {
+			System.out.println("30 question selected");
+		} else {
+		}
+		return list;
+	}
+
+}
